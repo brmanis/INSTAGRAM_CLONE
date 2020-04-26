e18264f8db7d29c46c05fb3e3283a477dbb92c81
ANIS <brmanis@gmail.com>
2020-04-26 12:19:14 +0100

    Get Data from Parse_Server

--- app/src/main/java/com/example/instagram_clone/MainActivity.java
+++ app/src/main/java/com/example/instagram_clone/MainActivity.java
@@ -6,29 +6,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
+
+import javax.security.auth.callback.Callback;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnSave;
+    private Button btnGetAllData;
     private EditText edtName, edtPSpeed,edtPPower,edtKSpeed,edtKPower;
-
+    private TextView txtData;
+    private String AllKickBoxers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnSave = findViewById(R.id.btnSave);
+        btnGetAllData=findViewById(R.id.btnGetAllData);
         edtName=findViewById(R.id.edtName);
         edtPSpeed=findViewById(R.id.edtPunchSpeed);
         edtPPower=findViewById(R.id.edtPunchPower);
         edtKSpeed=findViewById(R.id.edtKickSpeed);
         edtKPower=findViewById(R.id.edtKickPower);
+        txtData = findViewById(R.id.txtGetData);
+        txtData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery =ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("GTGIPmZQiJ", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object !=null && e ==null){
+                            txtData.setText(object.get("Name") + " - " + " Punch Power: " +object.get("punch_power") + " - " + " Punch Speed " +object.get("punch_speed") + " Kick Speed: " +object.get("kick_speed") + " Kick Power: " +object.get("kick_power") +" " );
+                        }
+                    }
+                });
+
+            }
+        });
+
         btnSave.setOnClickListener(this);
+        btnGetAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllKickBoxers = "";
+                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("KickBoxer");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e==null){
+
+                        }if(objects.size()>0){
+
+                            for(ParseObject kickBoxer: objects){
+                                AllKickBoxers= AllKickBoxers + kickBoxer.get("Name") + "\n";
+
+                            }
+                            FancyToast.makeText(MainActivity.this,AllKickBoxers,FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+
+                        }else{
+
+                            FancyToast.makeText(MainActivity.this,e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+
+                        }
+                    }
+                });
+            }
+        });
 
     }
 
@@ -46,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void done(ParseException e) {
                 if (e == null) {
-                    FancyToast.makeText(MainActivity.this,kickBoxer.get("name")+ "Has been successfully added to server",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                    FancyToast.makeText(MainActivity.this,kickBoxer.get("Name")+ "Has been successfully added to server",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
 
                 }else{
                     Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
